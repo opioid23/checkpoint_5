9aba159add473f0c7096eb8d7b1681a7e02b94a8
opioid23 <abdoumegdiche@icloud.com>
2020-06-16 11:51:23 +0100

    checkpoint commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import FullName from './Profile/FullName';
+import Address from './Profile/Address';
+import ProfilePhoto from './Profile/ProfilePhoto';
+export default function App() {
+    return (
+        <div>
+            <FullName/>
+            <Address/>
+            <ProfilePhoto/>
+        </div>
+    )
+}
---
+++ src/Profile/ProfilePhoto.js
@@ -0,0 +1,5 @@
+import React from 'react'
+const ProfilePhoto = () => {
+   return <img src="./public/logo192.jpg"/> 
+}
+export default  ProfilePhoto;
